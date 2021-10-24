f85ca15b5daab9ddad5b134e5e09f98c95ef4a77
Mikhail <16112000m@gmail.com>
2021-10-24 19:17:16 +0300

    refactor: Utils -> object

--- app/src/main/java/com/example/paranoid/utils/Utils.kt
+++ app/src/main/java/com/example/paranoid/utils/Utils.kt
@@ -3,10 +3,8 @@ package com.example.paranoid.utils
 import android.content.Context
 import android.widget.Toast
 
-class Utils {
-    companion object {
-        fun makeToast(context: Context, string: String) {
-            Toast.makeText(context, string, Toast.LENGTH_SHORT).show()
-        }
+object Utils {
+    fun makeToast(context: Context, string: String) {
+        Toast.makeText(context, string, Toast.LENGTH_SHORT).show()
     }
 }
