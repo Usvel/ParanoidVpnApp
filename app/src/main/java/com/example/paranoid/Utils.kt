900cf9e8c87be4309c772512d0d911c0c09ee7be
Mikhail <16112000m@gmail.com>
2021-10-24 19:02:11 +0300

    add: main screen design

---
+++ app/src/main/java/com/example/paranoid/Utils.kt
@@ -0,0 +1,12 @@
+package com.example.paranoid
+
+import android.content.Context
+import android.widget.Toast
+
+class Utils {
+    companion object{
+        fun makeToast(context: Context, string: String){
+            Toast.makeText(context, string, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/com/example/paranoid/VPNFragment.kt
+++ app/src/main/java/com/example/paranoid/VPNFragment.kt
@@ -1,8 +1,10 @@
 package com.example.paranoid
 
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.View
 import com.example.paranoid.databinding.NavigationVpnFragmentBinding
+
 
 class VPNFragment :
     BaseFragment<NavigationVpnFragmentBinding>(NavigationVpnFragmentBinding::inflate) {
@@ -10,6 +12,38 @@ class VPNFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.testTextView.text = getString(R.string.this_is_vpn)
+        // Just for test
+        loadMainConfiguration()
+        vpnButtonConnected()
+    }
+
+    private fun loadMainConfiguration() {
+        binding.mainConfigurationText.text = getString(R.string.test_first_configuration)
+        binding.mainConfigurationCard.setOnClickListener {
+            context?.let { context_ ->
+                Utils.makeToast(context_, getString(R.string.test_first_configuration))
+            }
+        }
+    }
+
+    private fun getVpnButtonColor(vpnButtonStateAttr: Int): Int {
+        val typedValue = TypedValue()
+        requireContext().theme.resolveAttribute(vpnButtonStateAttr, typedValue, true)
+        return typedValue.data
+    }
+
+    private fun vpnButtonDisable() {
+        binding.isConnected.text = getString(R.string.not_connected)
+        binding.vpnButtonBackground.background.setTint(getVpnButtonColor(R.attr.vpnButtonDisabled))
+    }
+
+    private fun vpnButtonConnected() {
+        binding.isConnected.text = getString(R.string.connected)
+        binding.vpnButtonBackground.background.setTint(getVpnButtonColor(R.attr.vpnButtonConnected))
+    }
+
+    private fun vpnButtonError() {
+        binding.isConnected.text = getString(R.string.error)
+        binding.vpnButtonBackground.background.setTint(getVpnButtonColor(R.attr.vpnButtonError))
     }
 }
