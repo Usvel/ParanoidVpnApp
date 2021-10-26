b241cc7d6c3262281a3c31e04c8915af29e524b2
khairov_ftl <hairovrm@sushivesla.team>
2021-10-26 23:54:18 +0300

    fix: package, status bar, navigation bar.

---
+++ app/src/main/java/com/paranoid/vpn/app/common/utils/Utils.kt
@@ -0,0 +1,47 @@
+package com.paranoid.vpn.app.common.utils
+
+import android.app.Application
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.StringRes
+import retrofit2.HttpException
+import java.net.ConnectException
+import java.net.NoRouteToHostException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+
+private const val NETWORK_CODE_500 = 500
+
+object Utils {
+
+    private var application: Application? = null
+
+    fun init(application: Application) {
+        Utils.application = application
+    }
+
+    fun Exception.isNetworkError(): Boolean {
+        return this is SocketTimeoutException ||
+            this is ConnectException ||
+            this is NoRouteToHostException ||
+            this is UnknownHostException ||
+            this is HttpException &&
+            this.code() >= NETWORK_CODE_500
+    }
+
+    fun getString(@StringRes id: Int, vararg parameters: Any): String {
+        return application?.getString(id, parameters)
+            ?: throw IllegalStateException(
+                "Application context in Utils not initialized.Please " +
+                    "call method init in your Application instance"
+            )
+    }
+
+    fun makeToast(context: Context, string: String) {
+        Toast.makeText(context, string, Toast.LENGTH_SHORT).show()
+    }
+}
+
+enum class VPNState {
+    CONNECTED, NOT_CONNECTED, ERROR
+}
