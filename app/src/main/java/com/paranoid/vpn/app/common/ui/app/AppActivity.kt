f5236ff642abe217213e0db0f8794f238b86d801
Mikhail <16112000m@gmail.com>
2021-11-14 19:24:39 +0300

    add: settings page

--- app/src/main/java/com/paranoid/vpn/app/common/ui/app/AppActivity.kt
+++ app/src/main/java/com/paranoid/vpn/app/common/ui/app/AppActivity.kt
@@ -2,15 +2,29 @@ package com.paranoid.vpn.app.common.ui.app
 
 import android.os.Build
 import android.os.Bundle
+import android.view.Window
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.paranoid.vpn.app.common.ui.base.BaseActivity
 import com.paranoid.vpn.app.databinding.ActivityMainBinding
+import android.view.WindowManager
+
+
+
 
 class AppActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setUpBottomNav(binding.bottomTabBar)
+
+        // TODO
+        //val w: Window = window // in Activity's onCreate() for instance
+//
+        //w.setFlags(
+        //    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+        //    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        //)
+
         // supportFragmentManager.beginTransaction().replace(R.id.my_nav_host_fragment, RegistrationFragment()).commit()
     }
 
--- app/src/main/java/com/paranoid/vpn/app/settings/ui/SettingsFragment.kt
+++ app/src/main/java/com/paranoid/vpn/app/settings/ui/SettingsFragment.kt
@@ -3,10 +3,16 @@ package com.paranoid.vpn.app.settings.ui
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
-import com.paranoid.vpn.app.R
 import com.paranoid.vpn.app.common.ui.base.BaseFragment
 import com.paranoid.vpn.app.common.utils.Utils
 import com.paranoid.vpn.app.databinding.NavigationSettingsFragmentBinding
+import android.view.animation.Animation
+
+import android.view.animation.LinearInterpolator
+
+import android.view.animation.RotateAnimation
+import com.paranoid.vpn.app.R
+
 
 class SettingsFragment :
     BaseFragment<NavigationSettingsFragmentBinding, SettingsViewModel>(
@@ -15,6 +21,19 @@ class SettingsFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.testTextView.text = Utils.getString(R.string.this_is_settings)
+
+        val rotate = RotateAnimation(
+            0F,
+            180F,
+            Animation.RELATIVE_TO_SELF,
+            0.5f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f
+        )
+        rotate.duration = 5000
+        rotate.interpolator = LinearInterpolator()
+        binding.settingsIcon.startAnimation(rotate)
+
     }
 
     override fun initViewModel() {
