abe9bb130c93db5c9783fb43dcd09ba8605ed336
Daria <shakhanova.ds@gmail.com>
2021-12-07 18:49:09 +0300

    feature/dasha/widget added widget

--- app/src/main/java/com/paranoid/vpn/app/common/ui/app/AppActivity.kt
+++ app/src/main/java/com/paranoid/vpn/app/common/ui/app/AppActivity.kt
@@ -1,6 +1,10 @@
 package com.paranoid.vpn.app.common.ui.app
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.Window
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.paranoid.vpn.app.common.ui.base.BaseActivity
 import com.paranoid.vpn.app.databinding.ActivityMainBinding
@@ -9,7 +13,24 @@ class AppActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infla
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+        Log.println(Log.DEBUG, "logs", "starter")
         setUpBottomNav(binding.bottomTabBar)
+        val extras = intent.extras
+        if (extras != null) {
+            Log.println(Log.DEBUG, "logs", "stARTER2")
+            if (extras.get("toQr") as Boolean) {
+                Log.println(Log.DEBUG, "logs", "toQr")
+            }
+            if (extras.get("toTurn") as Boolean) {
+                Log.println(Log.DEBUG, "logs", "toTurn")
+            }
+            if (extras.get("toService") as Boolean) {
+                Log.println(Log.DEBUG, "logs", "toService")
+            }
+            if (extras.get("toProxy") as Boolean) {
+                Log.println(Log.DEBUG, "logs", "toProxy")
+            }
+        }
         // TODO
         // supportFragmentManager.beginTransaction().replace(R.id.my_nav_host_fragment, RegistrationFragment()).commit()
         //window.setFlags(
---
+++ app/src/main/java/com/paranoid/vpn/app/widget/NewAppWidget.kt
@@ -0,0 +1,80 @@
+package com.paranoid.vpn.app.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+import android.app.PendingIntent
+
+import android.content.Intent
+import android.util.Log
+import com.paranoid.vpn.app.R
+import com.paranoid.vpn.app.common.ui.app.AppActivity
+
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class NewAppWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            Log.println(Log.INFO, "logs", "start")
+            val intentQr = Intent(context, AppActivity::class.java)
+            intentQr.putExtra("toQr", true)
+            intentQr.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            val pendingIntentQr = PendingIntent.getActivity(context, 0, intentQr, 0)
+            val viewsQr = RemoteViews(context.packageName, R.layout.new_app_widget)
+            viewsQr.setOnClickPendingIntent(R.id.to_qr, pendingIntentQr)
+
+            Log.println(Log.INFO, "logs", "second")
+            val intentTurn = Intent(context, AppActivity::class.java)
+            intentTurn.putExtra("toTurn", true)
+            intentTurn.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            val pendingIntentTurn = PendingIntent.getActivity(context, 0, intentTurn, 0)
+            val viewsTurn = RemoteViews(context.packageName, R.layout.new_app_widget)
+            viewsTurn.setOnClickPendingIntent(R.id.to_turn, pendingIntentTurn)
+
+            val intentService = Intent(context, AppActivity::class.java)
+            intentService.putExtra("toService", true)
+            intentService.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            val pendingIntentService = PendingIntent.getActivity(context, 0, intentService, 0)
+            val viewsService = RemoteViews(context.packageName, R.layout.new_app_widget)
+            viewsService.setOnClickPendingIntent(R.id.to_service, pendingIntentService)
+
+            val intentProxy = Intent(context, AppActivity::class.java)
+            intentProxy.putExtra("toProxy", true)
+            intentProxy.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            val pendingIntentProxy = PendingIntent.getActivity(context, 0, intentProxy, 0)
+            val viewsProxy = RemoteViews(context.packageName, R.layout.new_app_widget)
+            viewsProxy.setOnClickPendingIntent(R.id.to_list, pendingIntentProxy)
+
+            Log.println(Log.DEBUG, "logs", "proxy")
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.new_app_widget)
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
