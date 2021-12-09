b7fea02997246adb29a99c0ea555254e7229d9b6
Mikhail <16112000m@gmail.com>
2021-12-10 00:48:17 +0300

    refactor: Profile page

--- app/src/main/java/com/paranoid/vpn/app/profile/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/paranoid/vpn/app/profile/ui/profile/ProfileFragment.kt
@@ -3,7 +3,7 @@ package com.paranoid.vpn.app.profile.ui.profile
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isVisible
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
@@ -13,6 +13,11 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.paranoid.vpn.app.R
 import com.paranoid.vpn.app.common.Application
 import com.paranoid.vpn.app.common.ui.base.BaseFragment
@@ -36,10 +41,48 @@ class ProfileFragment :
         initDagger()
     }
 
+    private fun initGraph() {
+        val chart: BarChart = binding.bcGraph
+
+        val noOfEmp = ArrayList<BarEntry>()
+
+        noOfEmp.add(BarEntry(1040f, 0f))
+        noOfEmp.add(BarEntry(1040f, 1f))
+        noOfEmp.add(BarEntry(1133f, 2f))
+        noOfEmp.add(BarEntry(1240f, 3f))
+        noOfEmp.add(BarEntry(1369f, 4f))
+        noOfEmp.add(BarEntry(1487f, 5f))
+        noOfEmp.add(BarEntry(1501f, 6f))
+        noOfEmp.add(BarEntry(1645f, 7f))
+        noOfEmp.add(BarEntry(1578f, 8f))
+        noOfEmp.add(BarEntry(1695f, 9f))
+
+
+        val year = ArrayList<Any>()
+
+        year.add("2008")
+        year.add("2009")
+        year.add("2010")
+        year.add("2011")
+        year.add("2012")
+        year.add("2013")
+        year.add("2014")
+        year.add("2015")
+        year.add("2016")
+        year.add("2017")
+
+        val bardataset = BarDataSet(noOfEmp, "No Of Employee")
+        chart.animateY(5000)
+        val data = BarData(bardataset)
+        bardataset.setColors(*ColorTemplate.COLORFUL_COLORS)
+        chart.data = data
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setObservers()
         setListeners()
+        initGraph()
     }
 
     override fun onStop() {
@@ -57,7 +100,7 @@ class ProfileFragment :
         binding.cvProfileBasket.setOnClickListener {
             viewModel?.deleteUser()
         }
-        binding.cvProfileHange.setOnClickListener {
+        binding.viewChangeProfile.setOnClickListener {
             it.findNavController().navigate(R.id.action_profile_fragment_to_edit_user_fragment)
         }
         binding.cvProfileOut.setOnClickListener {
@@ -70,14 +113,30 @@ class ProfileFragment :
             it?.let {
                 when (it) {
                     UserLoggedState.USER_LOGGED_IN -> {
-                        binding.gProfileNotEnter.isVisible = true
-                        binding.gProfileButton.isVisible = false
-                        binding.tilProfilePassword.isVisible = false
+                        binding.cvGraph.visibility = View.VISIBLE
+                        binding.viewChangeProfile.visibility = View.VISIBLE
+                        binding.cvProfileBasket.visibility = View.VISIBLE
+                        binding.tvProfileStatistics.visibility = View.VISIBLE
+                        binding.cvProfileOut.visibility = View.VISIBLE
+
+                        binding.tvProfileEmail.visibility = View.VISIBLE
+                        binding.tvProfileEmailProfile.visibility = View.VISIBLE
+
+                        binding.cvProfileLogin.visibility = View.GONE
+                        binding.cvProfileRegistration.visibility = View.GONE
                     }
                     UserLoggedState.USER_LOGGED_OUT -> {
-                        binding.gProfileNotEnter.isVisible = false
-                        binding.gProfileButton.isVisible = true
-                        binding.tilProfilePassword.isVisible = false
+                        binding.cvGraph.visibility = View.GONE
+                        binding.viewChangeProfile.visibility = View.GONE
+                        binding.cvProfileBasket.visibility = View.GONE
+                        binding.cvProfileOut.visibility = View.GONE
+
+                        binding.tvProfileEmail.visibility = View.GONE
+                        binding.tvProfileEmailProfile.visibility = View.GONE
+
+                        binding.cvProfileLogin.visibility = View.VISIBLE
+                        binding.cvProfileRegistration.visibility = View.VISIBLE
+                        binding.tilProfilePassword.visibility = View.VISIBLE
                     }
                 }
             }
@@ -89,9 +148,9 @@ class ProfileFragment :
                 binding.tvProfileName.text = it.name
             }
             if (it.email.isNullOrEmpty()) {
-                binding.gProfileEmail.isVisible = false
+                binding.tvProfileEmail.visibility = View.GONE
+                binding.tvProfileEmailProfile.visibility = View.GONE
             } else {
-                binding.gProfileEmail.isVisible = true
                 binding.tvProfileEmailProfile.text = it.email
             }
             if (it.photoUrl.isNullOrEmpty()) {
@@ -139,9 +198,18 @@ class ProfileFragment :
         }
         viewModel?.statePasswordUser?.observe(viewLifecycleOwner) {
             if (it) {
-                binding.gProfileNotEnter.isVisible = false
-                binding.gProfileButton.isVisible = false
-                binding.tilProfilePassword.isVisible = true
+
+                binding.cvGraph.visibility = View.GONE
+                binding.viewChangeProfile.visibility = View.GONE
+                binding.cvProfileBasket.visibility = View.GONE
+                binding.cvProfileOut.visibility = View.GONE
+
+                binding.tvProfileEmail.visibility = View.VISIBLE
+                binding.tvProfileEmailProfile.visibility = View.VISIBLE
+
+                binding.cvProfileLogin.visibility = View.VISIBLE
+                binding.cvProfileRegistration.visibility = View.VISIBLE
+                binding.tilProfilePassword.visibility = View.GONE
 
                 setProceedBottomNav {
                     if (binding.etProfilePassword.text.isValidEmail()) {
